2c49c2c6a6ad51b0afb6d40d2bcc1102aab29cb5
Jori <jori.v.hanninen@gmail.com>
2023-04-04 13:08:20 +0300

    Backendin portti muutettu

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 
 //app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
@@ -105,6 +106,6 @@ app.get('/api/persons/:id', (req,res) => {
 	}
 })
 
-const PORT = 3001
+const PORT = process.env.port || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
